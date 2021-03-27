b42765b782376cd26cfbae17d3d4aa670735431b
charles kyalo <charleskyalo689@gmail.com>
2021-03-27 14:01:20 +0300

    add base game configuration

--- src/game.js
+++ src/game.js
@@ -1,1 +1,15 @@
 import Phaser from "phaser";
+import StartScene from './scenes/StartScene';
+const config = {
+  type: Phaser.AUTO,
+  width: 480,
+  height: 640,
+  physics: {
+    default: 'arcade',
+  },
+  backgroundColor: 'F8B392',
+  scene: [StartScene]
+
+}
+
+const game = new Phaser.Game(config);
