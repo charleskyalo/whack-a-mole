1c9b21e12c685dde0beacff079c368f37fd5a95a
charles kyalo <charleskyalo689@gmail.com>
2021-03-27 14:02:29 +0300

    create initial game scene

---
+++ src/scenes/StartScene.js
@@ -0,0 +1,24 @@
+import Phaser from 'phaser';
+
+class StartScene extends Phaser.Scene {
+  constructor() {
+    super({ key: 'StartScene' });
+  }
+  preload() {
+    this.load.image('startScreen', 'assets/whack-amole-epic.png')
+  }
+
+
+  create() {
+    const background = this.add.image(0, 0, 'startScreen');
+
+    background.setOrigin(0).setScale(0.5);
+
+    this.input.on("pointerup", () => {
+      /*   this.scene.start('GameScene');
+        this.scene.stop('StartScene'); */
+    })
+  }
+}
+
+export default StartScene;
